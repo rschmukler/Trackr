76f55f0490e7091b444069675205ee53bffe6b93
Ryan Schmukler <rschmukler@gmail.com>
2011-12-02 04:59:59 -0600

    Working Amazon

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,11 +1,13 @@
 class Carrier
   @@symbols = [:fedex, :ups, :usps]
 
-  def id_for_symbol(symbol)
-    @@symbols.index(symbol)
-  end
+  class << self
+    def id_for_symbol(symbol)
+      @@symbols.index(symbol)
+    end
 
-  def symbol_for_id(id)
-    @@symbols[id]
+    def symbol_for_id(id)
+      @@symbols[id]
+    end
   end
 end
